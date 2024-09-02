7e68bad5819cd00451048ab57c5f95c61345e550
imprakhartripathi <imprakhartripathiofficial@gmil.com>
2024-09-02 10:12:18 +0530

    Random Tries

---
+++ riwayat-frontend/src/dashboard/dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './dashboard.css';
+
+function dashboard(){
+    return(
+        <div className="container">
+            <h2>Welcome to The Riwayat Dashboard!</h2>
+        </div>
+    );
+}
+
+export default dashboard;
